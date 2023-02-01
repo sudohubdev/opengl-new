139e3dbca814fabf85dc355b27d061e335bd9377
Denis(mac) <denistesla2005@gmail.com>
2023-02-01 13:12:40 +0200

    final helloworld product

--- main.cpp
+++ main.cpp
@@ -1,17 +1,41 @@
-#include "linux.hpp"
-#define GLFW_DLL
-#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <OpenGL/gl.h>
 
+#include <iostream>
 int main()
 {
-
-    platspec_init();
-
-    do
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
     {
-        platspec_glclear();
-    } while (!platspec_termprog());
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
 
-    platspec_term();
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    
+    glViewport(0, 0, width, height);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
     return 0;
 }
